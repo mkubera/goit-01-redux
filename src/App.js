9bb596d89327cc655a371b99ce19f2ba373328e9
mkubera <rainteller@tutanota.de>
2022-01-04 21:03:43 +0100

    init + redux + counter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Counter from "./components/Counter";
+import { useSelector, useDispatch } from "react-redux";
 
-function App() {
+import { increment, decrement } from "./reducers/actions";
+
+const App = () => {
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Counter
+          value={counter}
+          onIncrement={() => dispatch(increment())}
+          onDecrement={() => dispatch(decrement())}
+        />
       </header>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Counter.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Counter = ({ value, onIncrement, onDecrement }) => {
+  console.log(onIncrement);
+  return (
+    <div>
+      <button onClick={onIncrement}>+1</button>
+      <p>{value}</p>
+      <button onClick={onDecrement}>-1</button>
+    </div>
+  );
+};
+
+export default Counter;
